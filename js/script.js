4dfa63909fd4ba2b8d688db608a956e6c6c322f8
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2024-11-11 20:29:02 +0100

    added timer and an event in button

--- js/script.js
+++ js/script.js
@@ -6,41 +6,21 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 */
 
-/*
--creare un array vuoto per i numeri da indovinare [numberToGuess = []]
--generare un numero random tra 1 e 100 5 volte e creare il li da appendere alla ul e appenderlo
-
-userNumber[]
-
-ipotesi counter
-
-numeriIndovinati[]
-
-
-premdiamo dall'html
-ul#numbers-list --> la lista dove devo mettere i li con i numeri
-
-form#answer-form --> form con evento submit o bottone o con evento click
-
-p#message --> per stampare il messaggio finale
-
-const inputs = document.querySelectorAll(input);
-for(let i = 0; i < inputs.lenght; i++) {
-    userNumber.push(inputs[i].value);
-}
-
-*/
 "use strict";
 console.clear();
 
 const numbersList = document.getElementById("numbers-list");
+const answerForm = document.getElementById("answers-form");
+let message = document.getElementById("message");
+const inputs = document.querySelectorAll("input");
+const button = document.getElementById("button");
 
 //Funzione per generare 5 numeri casuali senza ripetizioni e inserirli in un array
 function randomNum() {
     //Utilizzo un Set per evitare i duplicati
     const numberListElement = new Set();
     while (numberListElement.size < 5) {
-        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        const randomNumber = Math.floor(Math.random() * 50) + 1;
         numberListElement.add(randomNumber);
     }
     return Array.from(numberListElement);
@@ -54,4 +34,30 @@ for (let i = 0; i < randomNumbers.length; i++) {
 }
 
 numbersList.innerHTML = listItems;
-console.log(listItems)
+
+//Imposto il timer, dopo il quale spariscono i numeri e appaiono le caselle input 
+const timer = setTimeout(function() {
+    numbersList.classList.add("d-none");
+    answerForm.classList.remove("d-none");
+}, 10000);
+
+    //Aggiungo un evento nel bottone per inviare i dati
+    button.addEventListener('click', function(event){
+    event.preventDefault();
+    let correctNumbers = [];
+
+    //Ciclo gli input
+    for (let i = 0; i < inputs.length; i++) {
+        const userAnswer = parseInt(inputs[i].value);
+        if (randomNumbers.includes(userAnswer)) {
+            correctNumbers.push(userAnswer);
+    }
+  //Condizione che stabilisce se e quanti numeri l'utente ha indovinato  
+} if (correctNumbers.length > 0) {
+        message.textContent = `Hai indovinato i numeri: ${correctNumbers.join(", ")}.`;
+        message.classList.remove("text-danger");
+        message.classList.add("text-success");
+    } else {
+        message.textContent = "Non hai indovinato nessun numero.";
+    }
+})
